d7686823410516ddc285eb9a2a39183698e01f5e
jgregorio <gregoriojesus0@gmail.com>
2019-10-30 23:30:18 +0100

    datos guest

---
+++ src/app/mock.guests.ts
@@ -0,0 +1,6 @@
+import { Guest } from './guest';
+
+export const GUESTS: Guest[] = [
+    { "name": "Jesús", "from": "2015-12-26", "to": "2030-01-26", "index": 0 },
+    { "name": "Yan", "from": "2015-01-16", "to": "2030-02-16", "index": 1 }
+];
